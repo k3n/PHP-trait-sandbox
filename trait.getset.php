8ca2c9fea0fbbaf2ba91ab97d566eb037c94ed5c
k3n <github.com@kenman.net>
2012-02-15 23:37:21 -0600

    now with 50% less fat

--- trait.getset.php
+++ trait.getset.php
@@ -11,66 +11,35 @@
 trait GetSet {
 
     public function __call($methodCall, $args) {
-        $debug = !false;
 
-        if ($debug) {
-            printf('%s%s', str_pad(' ' . __TRAIT__ . ' ', 40, '-', STR_PAD_BOTH), PHP_EOL);
-            printf('%s->__call("%s", [argc: %d]);%s', __CLASS__, $methodCall, count($args), PHP_EOL);
+        $searchKey = substr($methodCall, 3);
+        $keys = array(lcfirst($searchKey), ucfirst($searchKey), $searchKey);
+
+        foreach ($keys as $key) {
+            if (property_exists($this, $key)) {
+                $theKey = $key;
+                break;
+            }
         }
 
-        static $fnFindKey;
-        if (!isset($fnFindKey)) {
-
-            if ($debug) {
-                printf('new closure $fnFindKey() for "%s".%s', __CLASS__, PHP_EOL);
-            }
-
-            $fnFindKey = function($searchKey, $debug) {
-
-                $objProps = array_keys(get_object_vars($this));
-
-                $keys = array(
-                    lcfirst($searchKey),
-                    ucfirst($searchKey),
-                    $searchKey
-                );
-
-                $theKey = '';
-                foreach ($keys as $i => $key) {
-                    if (in_array($key, $objProps)) {
-                        $theKey = $key;
-                        break;
-                    }
-                    if ($debug) {
-                        printf('missed: "%s"%s', $key, PHP_EOL);
-                    }
-                }
-
-                if ($debug) {
-                    printf('took %d iters.%s', $i + 1, PHP_EOL);
-                }
-
-                if (!strlen($theKey)) {
-                    throw new BadMethodCallException(sprintf('Missing "%s" in [%s] (search: [%s]).',
-                        $searchKey, implode(', ', $objProps), implode(', ', $keys)
-                    ));
-                }
-
-                return $theKey;
-            };
+        if (!isset($theKey)) {
+            throw new BadMethodCallException(sprintf('"%s" not found; tried: [%s].',
+                $searchKey, implode(', ', $keys)
+            ));
         }
 
         $method = substr($methodCall, 0, 3);
-        $op     = substr($methodCall, 3);
-        $key    = $fnFindKey($op, $debug);
-        $ret    = '';
-
-        if (($method == 'get') && isset($this->$key)) {
-            $ret = $this->$key;
-        }
-        else if (($method == 'set') && isset($args[0])) {
-            $this->$key = $args[0];
-            $ret = $this;
+        $return = '';
+        if ($method == 'get') {
+            $return = $this->$theKey;
+        } else if ($method == 'set') {
+            if (!isset($args[0])) {
+                throw new BadMethodCallException(sprintf('%s->%s(void), really?',
+                    __CLASS__, $methodCall
+                ));
+            }
+            $this->$theKey = $args[0];
+            $return = $this;
         } else {
             // we're only here because the method doesn't exist in the class, either.
             throw new BadMethodCallException(sprintf(
@@ -79,7 +48,7 @@ trait GetSet {
             ));
         }
 
-        return $ret;
+        return $return;
     }
 }
 

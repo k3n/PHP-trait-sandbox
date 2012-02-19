241b2053f8ea01363bf235213b7795c30a65abee
k3n <github.com@kenman.net>
2012-02-19 17:53:38 -0600

    ...

--- trait.logger.php
+++ trait.logger.php
@@ -2,56 +2,45 @@
 
 trait Logger
 {
-    public static function LogStatic($log, $logName = '')
-    {
+    public static function LogStatic($log, $logName = '') {
         static::write($log, $logName);
     }
 
-    public function Log($log, $logName = '')
-    {
+    public function Log($log, $logName = '') {
         static::write($log, $logName);
     }
 
-    protected static function write($log, $logName = '')
-    {
+    protected static function write($log, $logName = '') {
         $logEntry = sprintf('[%s] %s%s', date('d-M-y H:i:s T'), $log, PHP_EOL);
-        if (!file_put_contents(static::getLogPath($logName), $logEntry, FILE_APPEND))
-        {
+        if (!file_put_contents(static::getLogPath($logName), $logEntry, FILE_APPEND)) {
             throw new RuntimeException(sprintf('%s could not write to file "%s".', __TRAIT__, static::getLogPath($logName)));
         }
     }
 
-    final private static function getLogPath($logName = '')
-    {
+    final private static function getLogPath($logName = '') {
         static $logFiles;
 
-        if (!is_array($logFiles))
-        {
+        if (!is_array($logFiles)) {
             $logFiles = array();
         }
 
-        if (!strlen($logName))
-        {
+        if (!strlen($logName)) {
             $logName = 'default';
         }
 
-        if (!isset($logFiles[$logName]))
-        {
+        if (!isset($logFiles[$logName])) {
             $newPaths = array(__TRAIT__, __CLASS__);
             $dir = './';
-            foreach ($newPaths as $newPath)
-            {
+            foreach ($newPaths as $newPath) {
                 $dir .= $newPath;
-                if (!is_dir($dir) && !mkdir($dir))
-                {
+                if (!is_dir($dir) && !mkdir($dir)) {
                     throw new RuntimeException(sprintf('%s could not create directory "%s".', __TRAIT__, $dir));
                 }
                 $dir .= DIRECTORY_SEPARATOR;
             }
 
             $logFile = $dir . $logName  . '.log';
-            if (!is_file($logFile) && !touch($logFile))
-            {
+            if (!is_file($logFile) && !touch($logFile)) {
                 throw new RuntimeException(sprintf('%s could not create file "%s".', __TRAIT__, $logFile));
             }
 

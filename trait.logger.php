1c30375ff251831b929fb4335a6d81ee63f9fe18
k3n <github.com@kenman.net>
2012-02-19 17:50:23 -0600

    ack tabs vs spaces, or how i need to reconfig my text editor

--- trait.logger.php
+++ trait.logger.php
@@ -2,20 +2,20 @@
 
 trait Logger
 {
-	public static function LogStatic($log, $logName = '')
-	{
+    public static function LogStatic($log, $logName = '')
+    {
         static::write($log, $logName);
-	}
+    }
 
-	public function Log($log, $logName = '')
-	{
+    public function Log($log, $logName = '')
+    {
         static::write($log, $logName);
-	}
+    }
 
     protected static function write($log, $logName = '')
     {
         $logEntry = sprintf('[%s] %s%s', date('d-M-y H:i:s T'), $log, PHP_EOL);
-		if (!file_put_contents(static::getLogPath($logName), $logEntry, FILE_APPEND))
+        if (!file_put_contents(static::getLogPath($logName), $logEntry, FILE_APPEND))
         {
             throw new RuntimeException(sprintf('%s could not write to file "%s".', __TRAIT__, static::getLogPath($logName)));
         }
@@ -23,10 +23,10 @@ trait Logger
 
     final private static function getLogPath($logName = '')
     {
-		static $logFiles;
+        static $logFiles;
 
-		if (!is_array($logFiles))
-		{
+        if (!is_array($logFiles))
+        {
             $logFiles = array();
         }
 
@@ -35,8 +35,8 @@ trait Logger
             $logName = 'default';
         }
 
-		if (!isset($logFiles[$logName]))
-		{
+        if (!isset($logFiles[$logName]))
+        {
             $newPaths = array(__TRAIT__, __CLASS__);
             $dir = './';
             foreach ($newPaths as $newPath)
@@ -50,13 +50,13 @@ trait Logger
             }
 
             $logFile = $dir . $logName  . '.log';
-		    if (!is_file($logFile) && !touch($logFile))
+            if (!is_file($logFile) && !touch($logFile))
             {
                 throw new RuntimeException(sprintf('%s could not create file "%s".', __TRAIT__, $logFile));
             }
 
-		    $logFiles[$logName] = $logFile;
-		}
+            $logFiles[$logName] = $logFile;
+        }
 
         return $logFiles[$logName];
     }
